07312b56da7a1c7da301202b694be22ff514666f
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-03-11 12:26:05 -0400

    adding switch and redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { Switch, Redirect, HashRouter as Router, Route } from 'react-router-dom'
 import axios from 'axios'
 import Users from './Users'
 import Things from './Things'
@@ -35,8 +35,11 @@ class App extends Component{
             <div>           
                 <h1>Acme Favorites</h1>
                 <Route component={Nav} />
-                <Route path='/users' render={ ()=> <Users users={users}/> }/>
-                <Route path='/things' render={ ()=> <Things things={things}/> }/>  
+                <Switch>
+                    <Route path='/users' render={ ()=> <Users users={users}/> }/>
+                    <Route path='/things' render={ ()=> <Things things={things}/> }/> 
+                    <Redirect to='/users'/>
+                </Switch> 
             </div>
             </Router>
         )
